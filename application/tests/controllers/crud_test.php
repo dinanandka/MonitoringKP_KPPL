7fe51092f622937579873c6b6d7531df7aabae29
dinanandka <dinanandika@gmail.com>
2017-10-21 16:53:37 +0700

    Add files via upload

--- application/tests/controllers/crud_test.php
+++ application/tests/controllers/crud_test.php
@@ -10,7 +10,102 @@
 class Crud_test extends TestCase
 {	
    
-
+	public function test_tambah_aksi_berhasil()
+	{	
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '10/21/2017',
+					'topik' => 'topik hangat',
+					'isi' => 'laporan KP mingguan ini',
+					'dospem' => 'Bu wiwik',
+					'nama' => 'ervina'
+				]
+		);
+		$this->assertRedirect('crud/sukses', $output);
+	}
+	
+	public function test_tambah_aksi_gagaltanggal()
+	{
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '',
+					'topik' => 'topik hangat',
+					'isi' => 'laporan KP mingguan ini',
+					'dospem' => 'Bu wiwik',
+					'nama' => 'ervina'
+				]
+		);
+		$this->assertFalse( isset($_SESSION['username']) );
+	}
+	
+	public function test_tambah_aksi_gagaltopik()
+	{
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '10/21/2017',
+					'topik' => '',
+					'isi' => 'laporan KP mingguan ini',
+					'dospem' => 'Bu wiwik',
+					'nama' => 'ervina'
+				]
+		);
+		$this->assertFalse( isset($_SESSION['username']) );
+	}
+	
+	public function test_tambah_aksi_gagalisi()
+	{
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '10/21/2017',
+					'topik' => 'topik 1',
+					'isi' => '',
+					'dospem' => 'Bu wiwik',
+					'nama' => 'ervina'
+				]
+		);
+		$this->assertFalse( isset($_SESSION['username']) );
+	}
+	
+	public function test_tambah_aksi_gagaldospem()
+	{
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '10/21/2017',
+					'topik' => 'topik 1',
+					'isi' => 'belum ada aktivitas',
+					'dospem' => '',
+					'nama' => 'ervina'
+				]
+		);
+		$this->assertFalse( isset($_SESSION['username']) );
+	}
+	
+	public function test_tambah_aksi_gagalnama()
+	{
+		$this->request(
+			'POST',
+			'crud/tambah_aksi',
+				[
+					'tanggal' => '10/21/2017',
+					'topik' => 'topik 1',
+					'isi' => 'belum ada aktivitas',
+					'dospem' => 'Bu Tyas',
+					'nama' => ''
+				]
+		);
+		$this->assertFalse( isset($_SESSION['username']) );
+	}
+	
 	public function test_sukses()
 	{
 		$output = $this->request('GET', 'crud/sukses');
@@ -21,25 +116,6 @@ class Crud_test extends TestCase
 	{
 		$output = $this->request('GET', 'crud/gagal');
 	}
-
-	public function test_tambah_aksi()
-	{	
-		//$this->assertFalse( isset($_SESSION['eo']) );
-                $_SESSION['username'] = "username";
-		$this->request(
-			'POST',
-			'crud/tambah_aksi',
-				[
-					'tanggal' => '02/11/1997',
-					'topik' => 'topik hangat',
-					'isi' => 'laporan KP mingguan ini',
-					'dospem' => 'Bu wiwik',
-					'nama' => 'ervina'
-				]
-		);
-		$this->assertRedirect('crud/sukses');
-	}
-
 
 	public function test_hapus()
 	{	
