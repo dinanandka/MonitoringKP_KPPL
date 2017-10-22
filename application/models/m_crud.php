43e736a65e7beaec0a4c5e105b3594e2d51985a9
Agungprnm <gungpe97@gmail.com>
2017-10-22 16:41:13 +0700

    Update m_crud.php

--- application/models/m_crud.php
+++ application/models/m_crud.php
@@ -4,9 +4,7 @@
 		function input_data($data,$table){
 			$this->db->insert($table,$data);
 		}
-		function edit_data($where,$table){
-			return $this->db->get_where($table,$where);
-		}
+		
 
 		function update_data($where,$data,$table){
 		$this->db->where($where);
