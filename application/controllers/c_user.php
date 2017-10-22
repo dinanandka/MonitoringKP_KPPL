8e5f6d06a9d5a9fc517f976ff0ed9e88add6ebb8
dinanandka <dinanandika@gmail.com>
2017-10-22 14:14:52 +0700

    Update c_user.php

--- application/controllers/c_user.php
+++ application/controllers/c_user.php
@@ -5,8 +5,7 @@ class C_user extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		if ($this->session->userdata('level') != 'user') {
-			redirect('auth');
-		}
+			redirect('auth'); }
 		
 	}
 	public function index() {
