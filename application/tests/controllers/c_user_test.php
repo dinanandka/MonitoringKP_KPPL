b2fa1ca2914b982b1bb535682b688cc3d326d5be
dinanandka <dinanandika@gmail.com>
2017-10-22 14:04:11 +0700

    Update c_user_test.php

--- application/tests/controllers/c_user_test.php
+++ application/tests/controllers/c_user_test.php
@@ -31,16 +31,12 @@ class c_user_test extends TestCase {
 		$this->assertContains('<title>Laporan Kerja Praktik</title>', $output); //yg dituju ... index.html
 	}
 	
-	public function test_logout(){
-            $_SESSION['username'] = "username";
-            $this->request('GET', 'c_user/logout');
-            $this->assertRedirect('auth');
-        }
-            //public function test_logout(){
-            //$_SESSION['username'] = "username";
-            //$_SESSION['password'] = "password";
-            //$this->request('GET', 'c_user/logout');
-            //$this->assertRedirect('auth');
-        //}
+	public function test_logout(){ 
+           $_SESSION['username'] = "ervina"; 
+           $_SESSION['level'] = "user"; 
+           $_SESSION['logged_in'] = TRUE; 
+           $output = $this->request('GET','c_user/logout'); 
+           $this->assertRedirect('auth'); 
+		   }
 	
 }
