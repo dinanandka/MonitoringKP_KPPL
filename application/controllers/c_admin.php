36d22bc196e5ba58cf13f0b01e8010a0b5d1e09e
dinanandka <dinanandika@gmail.com>
2017-10-22 13:53:26 +0700

    Update c_admin.php

--- application/controllers/c_admin.php
+++ application/controllers/c_admin.php
@@ -29,7 +29,6 @@ class C_admin extends CI_Controller {
 		$this->session->unset_userdata('username');
 		$this->session->unset_userdata('level');
 		session_destroy();
-		redirect('auth');
-	}
+		redirect('auth'); }
 }
 ?>
