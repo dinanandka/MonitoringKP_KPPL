404acf0638648361056754f188fcb554aa6bd678
dinanandka <dinanandika@gmail.com>
2017-10-22 14:24:44 +0700

    Update c_admin.php

--- application/controllers/c_admin.php
+++ application/controllers/c_admin.php
@@ -5,8 +5,7 @@ class C_admin extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		if($this->session->userdata('level') != 'admin') {
-			redirect('auth');
-		}
+			redirect('auth'); }
 		
 	}
 	public function index() {
