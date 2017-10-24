2eb50e0ba3af432a7e23549de87d25dda88bf7a3
dinanandka <dinanandika@gmail.com>
2017-10-24 21:13:28 +0700

    Add files via upload

--- application/views/form.php
+++ application/views/form.php
@@ -45,8 +45,7 @@
 					<a class="navbar-brand" href="<?php echo base_url().'/index.php/c_user/';?>"><h1><img src="<?php echo base_url().'/asset_umum/images/logo1.png';?>" alt="logo"></h1></a> 
 				</div> 
 				<div class="collapse navbar-collapse"> 
-					<ul class="nav navbar-nav navbar-right">
-						<li class="scroll"><a href="<?php echo base_url('index.php/c_user/index');?>">Home</a></li> 
+					<ul class="nav navbar-nav navbar-right"> 
 						<li class="scroll active"><a href="#laporan">Laporan</a></li>
 						<li class="scroll"><a href="<?php echo base_url('index.php/c_user/logout');?>">Logout</a></li>
 					</ul> 
@@ -55,89 +54,60 @@
 		</div><!--/navbar--> 
 	</header> <!--/#navigation--> 
 
-	<section id="contact">
-		<div class="container">
-			<div class="row text-center clearfix">
-				<div class="col-sm-8 col-sm-offset-2">
-					<div class="contact-heading">
-						<h2 class="title-one">Laporan</h2>
-						<p>Laporan Kegiatan Harian Selama Kerja Praktik</p>
-					</div>
+<section id="laporan">
+	<div class="container">
+		<div class="text-center">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2 class="title-one">Laporan</h2>
+				<p>Tulis laporan mu dan submit!</p>
+			</div>
+		</div>
+		</div>
+					<div class="col-sm-12">
+								<div class="status alert alert-success" style="display: none"></div>
+									<form id="contact-form" class="contact" name="contact-form" method="post" action="<?php echo site_url('crud/tambah_aksi')?>">
+							            <fieldset>
+							             <div class="col-sm-3"> 
+							              <div class="form-group">
+							              <h5>Tanggal :</h5>
+							                <input placeholder="mm/dd/yyyy" name="tanggal" type="date" autofocus="" required data-validation-required-message="Tanggal harus di input"> 
+											<p class="help-block text-danger"></p>
+											</div>
+											<div class="form-group">
+							              <h5>Nama :</h5>
+							                <input value="<?php echo $this->session->userdata("username"); ?>" name="nama" type="text" autofocus="" required data-validation-required-message="Siapa nama anda?"> 
+											<p class="help-block text-danger"></p>
+											</div>
+											<div class="form-group">
+							              <h5>Nama Dosen Pembimbing :</h5>
+							                <input placeholder="Dosen Pembimbing" name="dospem" type="text" value="" required data-validation-required-message="Siapa nama dosen pembimbing anda?"> 
+											<p class="help-block text-danger"></p>
+											</div>
+							                </div>
+							                <br></br>
+							                <div class="col-sm-12"> 
+							                <div class="form-group"> 
+							              <h5>Judul :</h5>
+							                <textarea rows="1" cols="30" name="topik" type="text" value="" required data-validation-required-message="Judul harus dimasukkan"> </textarea></div>
+							                <div class="form-group">
+							              <h5>Isi :</h5>
+							                <textarea cols="100" rows="10"  name="isi" type="text" value="" required data-validation-required-message="Harus ada isinya"> </textarea></div></div>
+											<h5>Upload :</h5>
+							                <input name="file_up" type="file" value="" required data-validation-required-message="Siapa nama dosen pembimbing anda?"> 
+							              <br></br>
+							              <br></br>
+							              <div class="col-sm-12"> 
+							              <input class="btn btn-primary" type="submit" value="SUBMIT"></td>
+							              </div>
+							              <hr> 
+							            </fieldset>
+							        </form>
+						</div>
+				
 				</div>
 			</div>
 		</div>
-		<div class="container">
-			<div class="contact-details">
-				<div class="pattern"></div>
-					<div class="row text clearfix">
-
-						<div class="col-sm-12"> 
-							<div id="contact-form-section">
-								<div class="status alert alert-success" style="display: none"></div>
-									<form id="contact-form" class="contact" name="contact-form" method="post" action="<?php echo site_url('crud/tambah_aksi')?>">
-						            <fieldset>
-							            <div class="col-sm-12"> 
-							                <div class="form-group">
-							   		            <h5>Tanggal :</h5>
-								                <input placeholder=" mm/dd/yyyy" name="tanggal" type="date" autofocus="" required data-validation-required-message="Tanggal harus di input"> 
-								                <p class="help-block text-danger"></p>
-								            </div>
-								            <div class="form-group">
-								    	        <h5>Nama :</h5>
-								                <input value="<?php echo $this->session->userdata("username"); ?>" name="nama" type="text" autofocus="" required data-validation-required-message="Siapa nama anda?"> 
-												<p class="help-block text-danger"></p> 
-								     	    </div>
-							                <div class="form-group">
-								    	        <h5>Nama Dosen Pembimbing :</h5>
-									 	        <input placeholder=" Enter Nama Dosen" name="dospem" type="text" value="" required data-validation-required-message="Siapa nama dosen pembimbing anda?"> 
-									 	        <p class="help-block text-danger"></p>
-								     	    </div>
-								        </div>
-						                <br></br>
-							            <div class="col-sm-12"> 
-							                <div class="form-group"> 
-							           	        <h5>Judul :</h5>
-										        <textarea rows="1" cols="72" name="topik" type="text" value="" required data-validation-required-message="Judul harus dimasukkan"> </textarea>
-										    </div>
-									        <div class="form-group">
-									            <h5>Isi :</h5>
-									            <textarea cols="144" rows="5"  name="isi" type="text" value="" required data-validation-required-message="Harus ada isinya"> </textarea>
-									        </div>
-									    </div>
-										<br><br>
-							          	<div class="col-sm-12"> 
-							              	<input class="btn btn-primary" type="submit" value="SUBMIT"></td>
-						              	</div>
-						              	<hr>
-						              	<br><br> 						              	
-						            </fieldset>
-									</form>
-							</div>
-						</div>
-						
-						<div class="col-sm-12">
-							<div class="contact-address">
-								<div class="social-icons" style="text-align: center">
-								<br><br><br><br>
-					           	    <a href="https://ms-my.facebook.com/pages/Jurusan-Sistem-Informasi-ITS/149235835122966" class="facebook external" data-animate-hover="shake"><i class="fa fa-facebook"></i></a>
-					                <a href="https://www.instagram.com/hmsi_its/" class="instagram external" data-animate-hover="shake"><i class="fa fa-instagram"></i></a>
-					                <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=jurusan@is.its.ac.id&su=Hello&shva=1" class="email external" data-animate-hover="shake"><i class="fa fa-envelope"></i></a>	       
-								</div>
-							</div>
-						</div>
-
-					</div>
-			</div>
-		</div> 
-	</section> <!--/#contact--> 	
-
-	<footer id="footer"> 
-		<div class="container"> 
-			<div class="text-center"> 
-				<p>Copyright &copy; 2017 - <a href="http://is.its.ac.id/">Information System Department</a> | ITS </p> 
-			</div> 
-		</div> 
-	</footer> <!--/#footer-->  
+	</section> 
 
 
 	<script type="text/javascript" src="<?php echo base_url().'/asset_umum/js/jquery.js';?>"></script> 
