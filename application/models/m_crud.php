3ea1d94995341323afadb4b26176a319a07f9a85
Agungprnm <gungpe97@gmail.com>
2017-10-27 20:01:59 +0700

    Update m_crud.php

--- application/models/m_crud.php
+++ application/models/m_crud.php
@@ -6,10 +6,7 @@
 		}
 		
 
-		function update_data($where,$data,$table){
-		$this->db->where($where);
-		$this->db->update($table,$data);
-	}
+		
 		function hapus_data($where,$table){
 		$this->db->where($where);
 		$this->db->delete($table);
